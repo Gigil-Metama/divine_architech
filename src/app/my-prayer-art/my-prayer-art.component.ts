6f23bd50eb52cea7546b69f1c03cdfd5bff1cd5e
Charles <charles@smallxscale.com>
2023-02-28 12:46:34 +0800

    adding variation and download

--- src/app/my-prayer-art/my-prayer-art.component.ts
+++ src/app/my-prayer-art/my-prayer-art.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-my-prayer-art',
@@ -7,11 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyPrayerArtComponent implements OnInit {
   url = '/assets/sample-artwork.jpeg';
+  prayer: string | null = '';
+  busy = false;
+
+  constructor(private api: ApiService) {
+    //
+  }
 
   ngOnInit() {
     const url = localStorage.getItem('prayer');
-    if ( url != null) {
-      this.url = url; 
+    if ( url != null && url !== '') {
+      this.url = url;
+      this.prayer = localStorage.getItem('prayer-text');
+    }
+  }
+
+  imgNotFound() {
+    this.url = '/assets/sample-artwork.jpeg';
+    this.prayer = '';
+    localStorage.removeItem('prayer');
+    localStorage.removeItem('prayer-text');
+  }
+
+  imgLoaded() {
+    this.busy = false;
+  }
+
+  makeVariation() {
+    if (this.prayer != null && this.prayer != '') {
+      this.busy = true;
+      this.api.generatePrayer(this.prayer).subscribe({ 
+        next: (v) => {
+          if (v.success) {
+            if (v.result && v.result.url) {
+              localStorage.setItem('prayer', v.result.url);
+              this.url = v.result.url;
+            }
+          }
+        },
+        error: (e) => {
+          this.busy = false;
+        }
+      });
     }
   }
 }
